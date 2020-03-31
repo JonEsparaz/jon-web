57858b2ed70f1666f604609171e34a7e2267aa73
Jon Esparaz <jon.esparaz@gmail.com>
2020-03-31 01:44:57 -0400

    more text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="HomePage">
-      Jon Esparaz
+      <header className="MyName">
+      Jon Esparaz - Personal Website v0.1
+      </header>
+      <p>Hello there... I just moved in, so the place is still a work in progress.</p>
     </div>
   );
 }
