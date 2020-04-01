90968055212968f9dcef82cebac190bf0fd877ff
Jon Esparaz <jon.esparaz@gmail.com>
2020-04-01 02:20:52 -0400

    basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/build">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
