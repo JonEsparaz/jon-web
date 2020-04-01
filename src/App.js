54b35058da547717106c30968d7bd070fe0f3536
Jon Esparaz <jon.esparaz@gmail.com>
2020-04-01 00:27:04 -0400

    clean up and logos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Speedcubing from './components/Speedcubing';
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 function Footer() {
   return (
     <div className="FooterContainer">
-      <span><a href="https://twitter.com/JonEsparaz" rel="noopener noreferrer" target="_blank"><img className="FooterSocialImg" style={{ marginRight: "0.5vw", marginLeft: "3vw", marginTop: "2.5vw", marginBottom: "2.5vw", height: "30px"}} src="twitter.png" alt="Twitter Logo" /></a></span>
+      <span><a href="https://twitter.com/JonEsparaz" rel="noopener noreferrer" target="_blank"><img className="FooterSocialImg" style={{ marginRight: "0.5vw", marginLeft: "3vw", marginTop: "2.5vw", marginBottom: "2.5vw", height: "30px"}} src="twitter.svg" alt="Twitter Logo" /></a></span>
       <span><a href="https://github.com/JonEsparaz" rel="noopener noreferrer" target="_blank"><img className="FooterSocialImg" style={{ marginRight: "0.5vw", marginLeft: "3vw", marginTop: "2.5vw", marginBottom: "2.5vw", height: "30px"}} src="github.png" alt="GitHub Logo" /></a></span>
-      <span><a href="https://www.youtube.com/c/JonEsparaz" rel="noopener noreferrer" target="_blank"><img className="FooterSocialImg" style={{ marginRight: "0.5vw", marginLeft: "3vw", marginTop: "2.5vw", marginBottom: "2.5vw", height: "30px"}} src="youtube.png" alt="YouTube Logo" /></a></span>
-      <span><a href="https://www.linkedin.com/in/jonathanesparaz/" rel="noopener noreferrer" target="_blank"><img className="FooterSocialImg" style={{ marginRight: "0.5vw", marginLeft: "3vw", marginTop: "2.5vw", marginBottom: "2.5vw", height: "30px"}} src="linkedin.png" alt="LinkedIn Logo" /></a></span>
+      <span><a href="https://www.youtube.com/c/JonEsparaz" rel="noopener noreferrer" target="_blank"><img className="FooterSocialImg" style={{ marginRight: "0.5vw", marginLeft: "3vw", marginTop: "2.5vw", marginBottom: "2.5vw", height: "30px"}} src="youtube.svg" alt="YouTube Logo" /></a></span>
+      <span><a href="https://www.linkedin.com/in/jonathanesparaz/" rel="noopener noreferrer" target="_blank"><img className="FooterSocialImg" style={{ marginRight: "0.5vw", marginLeft: "3vw", marginTop: "2.5vw", marginBottom: "2.5vw", height: "30px"}} src="linkedin-256.png" alt="LinkedIn Logo" /></a></span>
 
     </div>
   );
