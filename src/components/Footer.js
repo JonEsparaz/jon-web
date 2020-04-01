f8d1f422ea080b196c96a084a21c271dc4c8f156
Jon Esparaz <jon.esparaz@gmail.com>
2020-04-01 00:00:18 -0400

    push footer and menu

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function Footer() {
+  return (
+    <div className="FooterContainer">
+      <span><a href="https://twitter.com/JonEsparaz" rel="noopener noreferrer" target="_blank"><img className="FooterSocialImg" style={{ marginRight: "0.5vw", marginLeft: "3vw", marginTop: "2.5vw", marginBottom: "2.5vw" }} src="/public/twitter.png" alt="Twitter Logo" /></a></span>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function Menu() {
+  return (
+    <div>
+      test
+    </div>
+  );
+}
+
+export default Menu;
