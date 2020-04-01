bccbfc43b580f4ae1a0f7f75c7fce6131cb0e8c8
Jon Esparaz <jon.esparaz@gmail.com>
2020-04-01 01:14:56 -0400

    stuff

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import './Menu.css';
 
-function Menu() {
-  return (
-    <div>
-      test
-    </div>
-  );
+const links = [
+  { href: '/speedcubing', text: 'Speedcubing' },
+  { href: '/contact', text: 'Contact' },
+
+];
+
+const createNavItem = ({ href, text, className }) => (
+  <NavItem>
+    <NavLink href={href} className={className}>{text}</NavLink>
+  </NavItem>
+);
+
+export default class Menu extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = { isOpen: false };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div className="MenuContainer">
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/"><img src="/mylogo.png" style={{height: "100px"}}></img></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              {links.map(createNavItem)}
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
-
-export default Menu;
