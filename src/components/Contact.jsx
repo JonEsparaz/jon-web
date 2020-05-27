cf687dd5b5007bc3a8ac532ad16a499b7731cdd8
Jon Esparaz <jon.esparaz@gmail.com>
2020-05-27 02:26:51 -0400

    add grid layouts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import './Contact.scss';
 function Contact() {
   return (
     <div className="ContactContainer">
-    <a className="ContactText" href = "mailto:jon.esparaz@gmail.com">Email</a>
+    <div>I'm still building a contact form. Feel free to email be directly: &nbsp;<a className="ContactText" href = "mailto:jon.esparaz@gmail.com"> jon.esparaz@gmail.com</a></div>
     </div>
   );
 }
