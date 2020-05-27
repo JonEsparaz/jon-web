74552075029a7d4f6d5a3d2789f7d3b44027e2b3
Jon Esparaz <jon.esparaz@gmail.com>
2020-05-27 14:30:47 -0400

    adjust home grid, spelling error

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import './Contact.scss';
 function Contact() {
   return (
     <div className="ContactContainer">
-    <div>I'm still building a contact form. Feel free to email be directly: &nbsp;<a className="ContactText" href = "mailto:jon.esparaz@gmail.com"> jon.esparaz@gmail.com</a></div>
+    <div>I'm still building a contact form. Feel free to email me directly: &nbsp;<a className="ContactText" href = "mailto:jon.esparaz@gmail.com"> jon.esparaz@gmail.com</a></div>
     </div>
   );
 }
