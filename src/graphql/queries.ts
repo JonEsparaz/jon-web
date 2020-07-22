01bd5e9427e2aa5d6e90ac1598c60b6a656a083c
Jon Esparaz <jon.esparaz@gmail.com>
2020-07-21 21:26:19 -0400

    api changes

---
+++ src/graphql/queries.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getPost = /* GraphQL */ `
+  query GetPost($id: ID!) {
+    getPost(id: $id) {
+      id
+      title
+      date
+      content
+      previewImage
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listPosts = /* GraphQL */ `
+  query ListPosts(
+    $filter: ModelPostFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        date
+        content
+        previewImage
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
