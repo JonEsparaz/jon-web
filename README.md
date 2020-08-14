601ab0649878a89fdcea27bc06bc20d995d249ae
Jon Esparaz <jon.esparaz@gmail.com>
2020-08-14 15:16:48 -0400

    package updates, fix readme

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ In the project directory, run:
 - `npm install`
 - `npm start`
 
-View the project in your browser at `localhost:3000`
+View the project in your browser at `localhost:3003`
 
 Check out beta releases at: [https://beta.jonesparaz.ca/](https://beta.jonesparaz.ca)
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Amplify from "aws-amplify";
+import Amplify from "@aws-amplify/core";
 import awsmobile from "./aws-exports";
 Amplify.configure(awsmobile);
 
